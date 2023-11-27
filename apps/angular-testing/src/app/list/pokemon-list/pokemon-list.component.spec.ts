27202ef61fba5f62d82310900b6a4ed7c8b371e1
iczajkow <igor.czajkowski@adtran.com>
2023-11-27 12:30:45 +0100

    Set up pokemon-list.component.spec.ts

--- apps/angular-testing/src/app/list/pokemon-list/pokemon-list.component.spec.ts
+++ apps/angular-testing/src/app/list/pokemon-list/pokemon-list.component.spec.ts
@@ -1,13 +1,32 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { PokemonListComponent } from './pokemon-list.component';
 
-describe('PokemonListComponent', () => {
+describe('PokemonListComponent With Dependency', () => {
   let component: PokemonListComponent;
   let fixture: ComponentFixture<PokemonListComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [PokemonListComponent],
+      declarations: [PokemonListComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(PokemonListComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
+
+describe('PokemonListComponent Without Dependency', () => {
+  let component: PokemonListComponent;
+  let fixture: ComponentFixture<PokemonListComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [PokemonListComponent],
     }).compileComponents();
 
     fixture = TestBed.createComponent(PokemonListComponent);
