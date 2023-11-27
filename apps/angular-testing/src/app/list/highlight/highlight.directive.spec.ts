04d3008fe0e399df20a074760e68a61f7e3e6680
iczajkow <igor.czajkowski@adtran.com>
2023-11-27 14:12:16 +0100

    Finish highlight.directive.spec.ts tests

--- apps/angular-testing/src/app/list/highlight/highlight.directive.spec.ts
+++ apps/angular-testing/src/app/list/highlight/highlight.directive.spec.ts
@@ -1,8 +1,31 @@
 import { HighlightDirective } from './highlight.directive';
+import { Component } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+
+@Component({
+  template: ` <p angularTestingHighlight>Highlight me!</p> `,
+})
+class HostTestComponent {}
 
 describe('HighlightDirective', () => {
-  it('should create an instance', () => {
-    const directive = new HighlightDirective();
-    expect(directive).toBeTruthy();
+  let fixture: ComponentFixture<HostTestComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [HighlightDirective, HostTestComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(HostTestComponent);
+  });
+
+  it('should add color style on mouse enter', () => {
+    fixture.debugElement
+      .query(By.directive(HighlightDirective))
+      .triggerEventHandler('mouseenter', null);
+    fixture.detectChanges();
+    expect(fixture.nativeElement.querySelector('p').style.backgroundColor).toBe(
+      'yellow'
+    );
   });
 });
